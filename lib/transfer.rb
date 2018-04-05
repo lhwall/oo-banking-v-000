20ab8e34780ce5488f944c676d94499fb7e5a0f9
Lucy Wall <lhwall@gmail.com>
2018-04-05 19:34:56 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,16 +14,13 @@ class Transfer
   end
   
   def execute_transaction
-    if self.valid?
-      if @status != "complete"
+    if @status != "complete"
         @sender.balance -= amount 
         @receiver.balance += amount 
         @status = "complete"
       end
     else 
-        @status = "rejected"
-        "Transaction rejected. Please check your account balance."
-    end 
+      ""
   end
   
   
