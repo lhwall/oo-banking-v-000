1f334970bd377ed48d2c3f7c6f1121c076e7c386
Lucy Wall <lhwall@gmail.com>
2018-04-05 19:35:18 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,7 +14,7 @@ class Transfer
   end
   
   def execute_transaction
-    if @status != "complete"
+    if self.valid && @status == "pending"
         @sender.balance -= amount 
         @receiver.balance += amount 
         @status = "complete"
